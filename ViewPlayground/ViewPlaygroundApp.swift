acc7f1e9a83030aa204427f05b04e454abe6a1db
ilham <ilham.perdana@investree.id>
2023-08-10 13:34:59 +0700

    Initial Commit

---
+++ ViewPlayground/ViewPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewPlaygroundApp.swift
+//  ViewPlayground
+//
+//  Created by Ilham Perdana Jalasena on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
